8e34f63384274909a664814c2937873ceb84ae2c
KneerazPandey <nirajpandeyniraj77@gmail.com>
2023-07-23 21:09:44 +0545

    Added responsiveness

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -13,54 +13,60 @@ import './profile.scss';
 
 const Profile = () => {
   return (
-    <div className='profile'>
-      <div className="images">
-        <img src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"alt="" className="cover" />
-        <img src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"alt="" className="profilePic" />
-      </div>
-      <div className="profileContainer">
-        <div className="userInfo">
-          <div className="left">
+    <div className="profile">
+    <div className="images">
+      <img
+        src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt=""
+        className="cover"
+      />
+      <img
+        src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+        alt=""
+        className="profilePic"
+      />
+    </div>
+    <div className="profileContainer">
+      <div className="uInfo">
+        <div className="left">
           <a href="http://facebook.com">
-              <FacebookTwoToneIcon />
-            </a>
-            <a href="http://facebook.com">
-              <InstagramIcon />
-            </a>
-            <a href="http://facebook.com">
-              <TwitterIcon />
-            </a>
-            <a href="http://facebook.com">
-              <LinkedInIcon />
-            </a>
-            <a href="http://facebook.com">
-              <PinterestIcon />
-            </a>
-          </div>
-          <div className="center">
-            <span>Jane Doe</span>
-            <div className="info">
-              <div className="item">
-                <PlaceIcon />
-                <span>USA</span>
-              </div>
-              <div className="item">
-                <LanguageIcon />
-                <span>novo.dev</span>
-              </div>
+            <FacebookTwoToneIcon />
+          </a>
+          <a href="http://facebook.com">
+            <InstagramIcon />
+          </a>
+          <a href="http://facebook.com">
+            <TwitterIcon />
+          </a>
+          <a href="http://facebook.com">
+            <LinkedInIcon />
+          </a>
+          <a href="http://facebook.com">
+            <PinterestIcon />
+          </a>
+        </div>
+        <div className="center">
+          <span>Jane Doe</span>
+          <div className="info">
+            <div className="item">
+              <PlaceIcon />
+              <span>USA</span>
             </div>
-            <button>follow</button>
+            <div className="item">
+              <LanguageIcon />
+              <span>lama.dev</span>
+            </div>
           </div>
-          <div className="right">
-            <EmailOutlinedIcon />
-            <MoreVertIcon />
-          </div>
+          <button>follow</button>
+        </div>
+        <div className="right">
+          <EmailOutlinedIcon />
+          <MoreVertIcon />
         </div>
       </div>
-      <div className="posts">
-        <Posts />
-      </div>
+    <Posts/>
     </div>
+  </div>
   )
 }
 
